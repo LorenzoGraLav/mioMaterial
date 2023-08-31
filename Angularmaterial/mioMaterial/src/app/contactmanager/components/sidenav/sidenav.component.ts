2212e0da99ae4b541ec868bff10a22ffe06b0d96
lorenzogralav <lorenzogra2022@libero.it>
2023-08-31 16:10:29 +0200

    Initial commit

---
+++ Angularmaterial/mioMaterial/src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -0,0 +1,64 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {Observable} from "rxjs";
+import {User} from "../../models/user";
+import {Router} from "@angular/router";
+import {MatSidenav} from "@angular/material/sidenav";
+import {Direction} from "@angular/cdk/bidi/directionality";
+
+const SMALL_WIDTH_BREAKPOINT = 720;
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+    public isScreenSmall!: boolean;
+
+    users!: Observable<User[]>
+    isDarkTheme: boolean = false;
+    dir: Direction = 'ltr';
+
+
+    constructor(
+      private breakpointObserver: BreakpointObserver,
+      private userService: UserService,
+      private router: Router) { }
+
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+
+    toggleTheme() {
+      this.isDarkTheme = !this.isDarkTheme;
+    }
+
+    toggleDir(){
+      this.dir = this.dir === 'ltr' ? 'rtl' : 'ltr';
+
+    }
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
+
+    this.users = this.userService.users;
+    this.userService.loadAll();
+
+   this.router.events.subscribe(() => {
+     if(this.isScreenSmall){
+       //TODO close our sidenav
+       this.sidenav.close();
+     }
+   });
+
+
+  }
+
+
+}
---
+++ Angularmaterial/mioMaterial/src/app/contactmanager/contactmanager-app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from "@angular/platform-browser";
+import {MatIconRegistry} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-contactmanager-app',
+  template: `
+   <app-sidenav></app-sidenav>
+  `,
+  styles: [
+  ]
+})
+export class ContactmanagerAppComponent implements OnInit {
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconSet(
+      sanitizer.bypassSecurityTrustResourceUrl('assets/avatars.svg')
+    );
+  }
+
+  ngOnInit(): void {
+  }
+
+}
